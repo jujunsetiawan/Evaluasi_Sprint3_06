6ed3cd968d284ecfc7fcaf8a1afc3130f356df06
Your Name <you@example.com>
2020-09-18 22:07:23 +0700

    evaluasi

---
+++ no6.js
@@ -0,0 +1,16 @@
+function mentorProgrammer (nama,status,asal) {
+    nama()
+    status()
+    asal()
+}
+function nama () {
+    console.log('nama mentor wandi prastama')
+}
+function status () {
+    console.log('status nya santri')
+}
+function asal () {
+    console.log('berasal dari kota prabumulih')
+}
+
+mentorProgrammer(nama,status,asal);
